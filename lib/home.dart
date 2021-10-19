d5f59373730bb8a3712dd5e03dffd1fa2b040695
aakzsh <aakashferrari@gmail.com>
2021-10-19 16:55:35 +0530

    add files

--- lib/home.dart
+++ lib/home.dart
@@ -30,6 +30,36 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                // Image.asset("images/")
+                SizedBox(
+                  height: 2,
+                ),
+                Column(
+                  children: <Widget>[
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text("Today's Stats")),
+                    Container(
+                      height: 100,
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 2,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:awestruck/home.dart';
+import 'home.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(App());
@@ -14,6 +15,16 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // primaryColor: Colors.pink,
+        brightness: Brightness.dark,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context)
+              .textTheme
+              .apply(bodyColor: Colors.white, displayColor: Colors.white),
+        ),
+      ),
       title: "Awestruck",
       home: Decide(),
     );
