0cf8b293cd95b3e95c5085883f94c486b46f00bd
shruti <shrutigupta5555@gmail.com>
2021-10-24 23:26:28 +0530

    added pedometer

--- lib/walk/walk_home.dart
+++ lib/walk/walk_home.dart
@@ -2,6 +2,7 @@ import 'package:awestruck/auth/login.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:awestruck/constant_widgets/palette.dart';
+import 'package:pedometer/pedometer.dart';
 
 class WalkHome extends StatefulWidget {
   @override
@@ -9,6 +10,57 @@ class WalkHome extends StatefulWidget {
 }
 
 class _WalkHomeState extends State<WalkHome> {
+  Stream<StepCount> _stepCountStream;
+  Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
@@ -62,10 +114,10 @@ class _WalkHomeState extends State<WalkHome> {
                           height: 20,
                         ),
                         Text(
-                          "698",
+                          _steps,
                           style: TextStyle(
                               fontSize: 36, fontWeight: FontWeight.bold),
-                        )
+                        ),
                       ],
                     ),
                   ),
@@ -74,18 +126,54 @@ class _WalkHomeState extends State<WalkHome> {
                   backgroundColor: Color(0xff222D48),
                 ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
                     Container(
                       decoration: BoxDecoration(
                           color: Color(0xff151D31),
                           borderRadius: BorderRadius.circular(10)),
-                      child: Column(
-                        children: [
-                          Text(
-                            "Goals",
-                            style: TextStyle(fontSize: 12),
-                          )
-                        ],
+                      width: 0.4 * w,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Goals",
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            Text(
+                              "1000",
+                              style: TextStyle(
+                                  fontSize: 36,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff86EDFB)),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xff151D31),
+                          borderRadius: BorderRadius.circular(10)),
+                      width: 0.4 * w,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Coins",
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            Text(
+                              "700",
+                              style: TextStyle(
+                                  fontSize: 36,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff86EDFB)),
+                            )
+                          ],
+                        ),
                       ),
                     )
                   ],
