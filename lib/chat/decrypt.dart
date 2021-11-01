27dc1c9aa16ea6637fab1349ae79e36ecf6f065d
shruti <shrutigupta5555@gmail.com>
2021-11-01 21:20:26 +0530

    decrypt ui

--- lib/chat/decrypt.dart
+++ lib/chat/decrypt.dart
@@ -1,5 +1,6 @@
 import 'package:awestruck/constant_widgets/palette.dart';
 import 'package:awestruck/home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
@@ -15,6 +16,21 @@ class Decrypt extends StatefulWidget {
 
 class _DecryptState extends State<Decrypt> {
   Codec<String, String> stringToBase64 = utf8.fuse(base64);
+  String pfpurl;
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(widget.name)
+        .get()
+        .then((value) {
+      setState(() {
+        pfpurl = value.data()['pfp'];
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     String decoded = stringToBase64.decode(widget.msg);
@@ -22,32 +38,39 @@ class _DecryptState extends State<Decrypt> {
       body: Container(
         decoration: BoxDecoration(color: Palette().bluebg),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            ListTile(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text("Sent an aurora "),
-                  Text(
-                    widget.time.toString().substring(0, 16),
-                    style: TextStyle(fontSize: 12, color: Colors.white54),
-                  )
-                ],
-              ),
-              leading: CircleAvatar(
-                backgroundColor: Colors.pink,
-                radius: 20,
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 44.0),
+              child: ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Sent an aurora "),
+                    Text(
+                      widget.time.toString().substring(0, 16),
+                      style: TextStyle(fontSize: 12, color: Colors.white54),
+                    )
+                  ],
+                ),
+                leading: CircleAvatar(
+                  backgroundImage: MemoryImage(base64Decode(pfpurl)),
+                  radius: 20,
+                ),
               ),
             ),
             SizedBox(
               height: 30,
             ),
             Padding(
-              padding: EdgeInsets.all(16),
+              padding: EdgeInsets.all(24),
               child: Container(
-                decoration: BoxDecoration(color: Color(0xff0C1324)),
-                child: Text(decoded),
+                alignment: Alignment.topLeft,
+                decoration: BoxDecoration(color: Color(0xff16213A)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(decoded),
+                ),
               ),
             )
           ],
